32518859ca80cfbb8045d9c3996dbcb061730002
khushboo lal <138516471+khushhboo16@users.noreply.github.com>
2025-01-28 11:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import turtle as t
 import random
 pig= t.Turtle()
-colours=["red", "dark orchid" , "dark blue" , "orange red" , "purple" , "yellow" , "lime"]
+
 directions = [0, 90, 180, 270]
 pig.pensize(15)
+t.colormode(255)
 
+def random_color():
+  r= random.randint(0,255)
+  g= random.randint(0,255)
+  b= random.randint(0,255)
+  random_color=(r,g,b)
+  return random_color
 for i in range(200):
-  pig.color(random.choice(colours))
+  pig.color(random_color())
   pig.forward(30)
   pig.setheading(random.choice(directions))
+  
